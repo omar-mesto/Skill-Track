0e47e55807db7fa3b4680a2d2104236facaea877
omar-mesto <omisto014@gmail.com>
2025-11-21 04:09:20 +0300

    Create Followers for Professor

---
+++ components/Profile/ProfileProfessorTabs.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import Posts from '@/components/Profile/tabs/Posts.vue'
+import Followers from '@/components/Profile/tabs/Followers.vue'
+
+const { profile } = useProfessorProfile()
+
+const tabs = [
+  { name: 'Posts', component: Posts },
+  { name: 'Follow', component: Followers },
+]
+
+const activeTab = ref(tabs[0])
+</script>
+
+<template>
+  <div class="flex gap-4 border-b pb-2 px-1 mt-6 overflow-x-auto whitespace-nowrap">
+    <button
+      v-for="tab in tabs"
+      :key="tab.name"
+      :class="[
+        'pb-2 font-semibold transition-all duration-300',
+        activeTab.name === tab.name
+          ? 'text-primary border-b-2 border-primary'
+          : 'text-primary hover:text-Sequand hover:cursor-pointer',
+      ]"
+      @click="activeTab = tab"
+    >
+      {{ tab.name }}
+    </button>
+    <h1 class="text-red-500">
+      {{ profile?.user?._id }}
+    </h1>
+  </div>
+
+  <component
+    :is="activeTab.component"
+    :user-id="profile?.user?._id"
+  />
+</template>
